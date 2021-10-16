e62ac368b5f8f0f506d637b3b372506fadd0c32c
Natalia Petyaeva <npetyaeva@gmail.com>
2021-10-16 20:02:55 +0300

    Modified project files

---
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -0,0 +1,42 @@
+package ru.netology.statistic;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class StatisticsServiceTest {
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data.csv", delimiter = ';', numLinesToSkip = 1)
+    void souldCalculateSum(String purchases, long expected) {
+
+        StatisticsService service = new StatisticsService();
+
+        long actual = service.calculateSum(strToLong(purchases));
+
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data.csv", delimiter = ';', numLinesToSkip = 2)
+    void souldFindMax(String purchases, long expected) {
+
+        StatisticsService service = new StatisticsService();
+
+        long actual = service.findMax(strToLong(purchases));
+
+        assertEquals(expected, actual);
+    }
+
+    //Перевод строки в массив long
+    private long[] strToLong(String purchases) {
+        String[] strPurchases = purchases.split(",");
+
+        long[] longPurchases = new long[strPurchases.length];
+        for (int i = 0; i < strPurchases.length; i++) {
+            longPurchases[i] = Long.parseLong(strPurchases[i]);
+        }
+        return longPurchases;
+    }
+}
